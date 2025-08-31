334b9b751f2b11d075efd7e7bd29eb1856ac6a60
Andrew <bansheedriver400@hotmail.com>
2025-08-30 20:55:01 -0500

    Higher quality cards Ashes
    Use ashesDB as first image attempt, fallback to ashes.live

--- plugins/ashes_reborn/ashes.py
+++ plugins/ashes_reborn/ashes.py
@@ -3,6 +3,7 @@ from requests import Response, get
 from time import sleep
 
 CARD_ART_URL_TEMPLATE = 'https://cdn.ashes.live/images/cards/{card_stub}.jpg'
+CARD_ART_URL_PHG_TEMPLATE = 'https://ashesdb-media.plaidhatgames.com/images/new-cards/{card_stub}.jpg'
 DECK_URL_TEMPLATE = 'https://api.ashes.live/v2/decks/shared/{share_id}'
 
 def request_ashes(query: str) -> Response:
@@ -27,7 +28,11 @@ def fetch_deck_data(deck_id: str):
 
 def fetch_card_art(index: int, card_name: str, card_stub: str, quantity: int, front_img_dir: str):
 
-    card_art = request_ashes(CARD_ART_URL_TEMPLATE.format(card_stub=card_stub)).content
+    try:
+        card_stub_alt = card_stub.replace('-', '_')
+        card_art = request_ashes(CARD_ART_URL_PHG_TEMPLATE.format(card_stub=card_stub_alt)).content
+    except:
+        card_art = request_ashes(CARD_ART_URL_TEMPLATE.format(card_stub=card_stub)).content
 
     if card_art is not None:
         # Save image based on quantity
